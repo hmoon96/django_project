25a0f528f2daa711062a035a35ba385efbabaddf
hmoon96 <hlmooney98@gmail.com>
2025-02-25 18:47:32 +0700

    modified views.py file to hopefully fix index request and allow hello deployed page to work

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(requests):
+def index(request):
+
     if request.method == "POST":
-       return HttpResponse("You must have POSTed something")
+        return HttpResponse("You must have POSTed something")
     else:
         return HttpResponse(request.method)
